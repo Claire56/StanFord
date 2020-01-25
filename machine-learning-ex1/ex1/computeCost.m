30121a0d95a427176219141e2a6b840b6650e50f
claire muwanguzi <kclaire56@yahoo.com>
2020-01-25 14:55:46 -0800

    finished compute Cost

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,6 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+prediction = X * theta
+mse = (prediction-y).^2
+J = 1/(2*m) * sum(mse) 
 
 
 
